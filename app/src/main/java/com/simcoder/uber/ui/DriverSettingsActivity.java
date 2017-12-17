f8b9c8829ceb1bf0e2f462d335e730644006bf8a
MinaSamirCap <MenaSamerCap@gmail.com>
2017-12-17 13:42:09 +0200

    add loaders to driver profile

--- app/src/main/java/com/simcoder/uber/ui/DriverSettingsActivity.java
+++ app/src/main/java/com/simcoder/uber/ui/DriverSettingsActivity.java
@@ -28,6 +28,7 @@ import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
 import com.rengwuxian.materialedittext.MaterialEditText;
 import com.simcoder.uber.R;
+import com.victor.loading.rotate.RotateLoading;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
@@ -36,25 +37,18 @@ import java.util.Map;
 
 public class DriverSettingsActivity extends AppCompatActivity {
 
-    private MaterialEditText mNameField, mPhoneField, mCarField;
-
+    private MaterialEditText nameEditText, phoneEditText, carTypeEditText;
     private Button cancelButton, confirmButton;
-
-    private ImageView mProfileImage, adsImageView;
+    private ImageView profileImageView, adsImageView;
+    private RadioGroup radioGroup;
+    private RotateLoading rotateLoading;
 
     private FirebaseAuth mAuth;
     private DatabaseReference mDriverDatabase;
 
-    private String userID;
-    private String mName;
-    private String mPhone;
-    private String mCar;
-    private String mService;
-    private String mProfileImageUrl;
-
+    private String userID, nameString, phoneString,
+            carTypeString, serviceTypeString, profileImageUrlSting;
     private Uri resultUri;
-
-    private RadioGroup mRadioGroup;
 
 
     @Override
@@ -62,15 +56,15 @@ public class DriverSettingsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_driver_settings);
 
+        rotateLoading = findViewById(R.id.rotate_loading);
+        nameEditText = findViewById(R.id.name_edit_text);
+        phoneEditText = findViewById(R.id.phone_edit_text);
+        carTypeEditText = findViewById(R.id.car);
 
-        mNameField = findViewById(R.id.name_edit_text);
-        mPhoneField = findViewById(R.id.phone_edit_text);
-        mCarField = findViewById(R.id.car);
         adsImageView = findViewById(R.id.ads_image_view);
-        mProfileImage = findViewById(R.id.profile_image_view);
+        profileImageView = findViewById(R.id.profile_image_view);
 
-        mRadioGroup = findViewById(R.id.radioGroup);
-
+        radioGroup = findViewById(R.id.radio_group);
         cancelButton = findViewById(R.id.cancel_button);
         confirmButton = findViewById(R.id.confirm_button);
 
@@ -78,14 +72,10 @@ public class DriverSettingsActivity extends AppCompatActivity {
         userID = mAuth.getCurrentUser().getUid();
         mDriverDatabase = FirebaseDatabase.getInstance().getReference().child("Users").child("Drivers").child(userID);
 
-        getUserInfo();
-
-        mProfileImage.setOnClickListener(new View.OnClickListener() {
+        profileImageView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(Intent.ACTION_PICK);
-                intent.setType("image/*");
-                startActivityForResult(intent, 1);
+                openImageIntent();
             }
         });
 
@@ -100,56 +90,60 @@ public class DriverSettingsActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 finish();
-                return;
             }
         });
 
+        getUserInfo();
         loadAds();
     }
 
-    private void loadAds() {
-        Glide.with(getApplication())
-                .load(getString(R.string.driver_ad))
-                .into(adsImageView);
+    private void openImageIntent() {
+        Intent intent = new Intent(Intent.ACTION_PICK);
+        intent.setType("image/*");
+        startActivityForResult(intent, 1);
     }
 
     private void getUserInfo() {
+
+        rotateLoading.start();
+
         mDriverDatabase.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
                 if (dataSnapshot.exists() && dataSnapshot.getChildrenCount() > 0) {
                     Map<String, Object> map = (Map<String, Object>) dataSnapshot.getValue();
                     if (map.get("name") != null) {
-                        mName = map.get("name").toString();
-                        mNameField.setText(mName);
+                        nameString = map.get("name").toString();
+                        nameEditText.setText(nameString);
                     }
                     if (map.get("phone") != null) {
-                        mPhone = map.get("phone").toString();
-                        mPhoneField.setText(mPhone);
+                        phoneString = map.get("phone").toString();
+                        phoneEditText.setText(phoneString);
                     }
                     if (map.get("car") != null) {
-                        mCar = map.get("car").toString();
-                        mCarField.setText(mCar);
+                        carTypeString = map.get("car").toString();
+                        carTypeEditText.setText(carTypeString);
                     }
                     if (map.get("service") != null) {
-                        mService = map.get("service").toString();
-                        switch (mService) {
+                        serviceTypeString = map.get("service").toString();
+                        switch (serviceTypeString) {
                             case "One":
-                                mRadioGroup.check(R.id.one_radio_button);
+                                radioGroup.check(R.id.one_radio_button);
                                 break;
                             case "2 - 3":
-                                mRadioGroup.check(R.id.two_radio_button);
+                                radioGroup.check(R.id.two_radio_button);
                                 break;
                             case "Group":
-                                mRadioGroup.check(R.id.group_radio_button);
+                                radioGroup.check(R.id.group_radio_button);
                                 break;
                         }
                     }
                     if (map.get("profileImageUrl") != null) {
-                        mProfileImageUrl = map.get("profileImageUrl").toString();
-                        Glide.with(getApplication()).load(mProfileImageUrl).into(mProfileImage);
+                        profileImageUrlSting = map.get("profileImageUrl").toString();
+                        Glide.with(getApplication()).load(profileImageUrlSting).into(profileImageView);
                     }
                 }
+                rotateLoading.stop();
             }
 
             @Override
@@ -158,27 +152,29 @@ public class DriverSettingsActivity extends AppCompatActivity {
         });
     }
 
-
     private void saveUserInformation() {
-        mName = mNameField.getText().toString();
-        mPhone = mPhoneField.getText().toString();
-        mCar = mCarField.getText().toString();
 
-        int selectId = mRadioGroup.getCheckedRadioButtonId();
+        rotateLoading.start();
 
-        final RadioButton radioButton = (RadioButton) findViewById(selectId);
+        nameString = nameEditText.getText().toString();
+        phoneString = phoneEditText.getText().toString();
+        carTypeString = carTypeEditText.getText().toString();
+
+        int selectId = radioGroup.getCheckedRadioButtonId();
+
+        final RadioButton radioButton = findViewById(selectId);
 
         if (radioButton.getText() == null) {
             return;
         }
 
-        mService = radioButton.getText().toString();
+        serviceTypeString = radioButton.getText().toString();
 
         Map userInfo = new HashMap();
-        userInfo.put("name", mName);
-        userInfo.put("phone", mPhone);
-        userInfo.put("car", mCar);
-        userInfo.put("service", mService);
+        userInfo.put("name", nameString);
+        userInfo.put("phone", phoneString);
+        userInfo.put("car", carTypeString);
+        userInfo.put("service", serviceTypeString);
         mDriverDatabase.updateChildren(userInfo);
 
         if (resultUri != null) {
@@ -199,8 +195,8 @@ public class DriverSettingsActivity extends AppCompatActivity {
             uploadTask.addOnFailureListener(new OnFailureListener() {
                 @Override
                 public void onFailure(@NonNull Exception e) {
+                    rotateLoading.stop();
                     finish();
-                    return;
                 }
             });
             uploadTask.addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
@@ -211,12 +207,12 @@ public class DriverSettingsActivity extends AppCompatActivity {
                     Map newImage = new HashMap();
                     newImage.put("profileImageUrl", downloadUrl.toString());
                     mDriverDatabase.updateChildren(newImage);
-
+                    rotateLoading.stop();
                     finish();
-                    return;
                 }
             });
         } else {
+            rotateLoading.stop();
             finish();
         }
 
@@ -228,7 +224,14 @@ public class DriverSettingsActivity extends AppCompatActivity {
         if (requestCode == 1 && resultCode == Activity.RESULT_OK) {
             final Uri imageUri = data.getData();
             resultUri = imageUri;
-            mProfileImage.setImageURI(resultUri);
+            profileImageView.setImageURI(resultUri);
         }
     }
+
+    private void loadAds() {
+        Glide.with(getApplication())
+                .load(getString(R.string.driver_ad))
+                .into(adsImageView);
+    }
+
 }
